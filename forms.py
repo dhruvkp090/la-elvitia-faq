8b39eea3d002a0d9090748352246435ff0cfa724
dhruvkp090 <45363265+dhruvkp090@users.noreply.github.com>
2019-09-12 16:41:27 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class FAQform(FlaskForm):
     Question = StringField('Question', validators=[DataRequired()])
-    Answer = StringField('Answer', validators=[DataRequired()])
+    Answer = TextAreaField('Answer', validators=[DataRequired()])
     submit = SubmitField('Add')
